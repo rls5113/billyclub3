e0c5ab35f032aca8755b02cddcbde2edd33cda49
rls5113 <stuartrl@comcast.net>
2023-02-16 14:40:37 -0600

    spring initializer

---
+++ src/main/java/com/billyclub/points/Billyclub3Application.java
@@ -0,0 +1,13 @@
+package com.billyclub.points;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Billyclub3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Billyclub3Application.class, args);
+    }
+
+}
